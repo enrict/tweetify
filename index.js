9495ec070cb6abe93bad67ecc374da64e68eb452
enrict <enric_trillo@outlook.com>
2021-01-02 11:34:08 +0000

    created apollo server and connected to mongo dbase

---
+++ index.js
@@ -0,0 +1,23 @@
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    // start listening on port 5000
+    console.log("mongodb connected");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`server running at ${res.url}`);
+  });
