0939bb26ceeba41b7a07c88c1c6098dfd3d437cf
enrict <enric_trillo@outlook.com>
2021-01-03 17:47:40 +0000

    added func to display pposts from dB

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,56 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { Grid, Image } from "semantic-ui-react";
+import PostCard from "../components/PostCard";
 
 function Home() {
+  const {
+    loading,
+    data: { getPosts: posts },
+  } = useQuery(FETCH_POSTS_QUERY);
+
   return (
-    <div className="home">
-      <h2>home</h2>
-    </div>
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Your Timeline</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>loding posts...</h1>
+        ) : (
+          posts &&
+          posts.map((post) => (
+            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
   );
 }
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      commentCount
+      likes {
+        username
+      }
+      comments {
+        id
+        username
+        body
+        createdAt
+      }
+    }
+  }
+`;
 
 export default Home;
